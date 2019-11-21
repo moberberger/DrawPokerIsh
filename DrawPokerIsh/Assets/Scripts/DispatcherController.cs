330d326c8501dffe72d3603e9028adfa6d89ed4e
Michael Oberberger <52931052+moberberger@users.noreply.github.com>
2019-11-20 17:21:21 -0800

    Update DispatcherController.cs
    cleanup

--- DrawPokerIsh/Assets/Scripts/DispatcherController.cs
+++ DrawPokerIsh/Assets/Scripts/DispatcherController.cs
@@ -1,29 +1,27 @@
-﻿
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using System.Linq;
-using System;
-using DG;
-using DG.Tweening;
-using DG.Tweening.Core;
+﻿using System.Collections.Generic;
+
 using Morpheus;
-using System.Collections;
+
+using UnityEngine;
+
+
 public class DispatcherController : MonoBehaviour
 {
     private List<MessageHandler> m_forDeregistering = new List<MessageHandler>();
-    private void Awake()
+
+    void Awake()
     {
         Debug.Log( "Awake- Registering Handlers" );
         var discovery = new MessageHandlerDiscovery( Dispatcher.Default );
 
         foreach (var mb in Resources.FindObjectsOfTypeAll<MonoBehaviour>())
         {
-            m_forDeregistering.AddRange( discovery.RegisterInstanceHandlers( mb ) );
+            var registeredHandlers = discovery.RegisterInstanceHandlers( mb );
+            m_forDeregistering.AddRange( registeredHandlers );
         }
     }
 
-    private void OnDestroy()
+    void OnDestroy()
     {
         Debug.Log( "OnDestroy- Deregistering Handlers" );
         foreach (var handler in m_forDeregistering)
