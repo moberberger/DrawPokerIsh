7453b47c57b0f7c201b922b23946622558e09c63
Michael Oberberger <mike@oberberger.com>
2019-12-09 13:59:46 -0600

    Grew to 5 hands

---
+++ DrawPokerIsh/Assets/Scripts/HandBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandBehavior : MonoBehaviour
+{
+    public int Index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
