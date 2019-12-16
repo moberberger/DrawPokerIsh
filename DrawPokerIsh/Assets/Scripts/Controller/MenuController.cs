4fa9be6f3793f46fc897cec0f654f52a7f8dc1ae
Michael Oberberger <mike@oberberger.com>
2019-12-16 10:43:38 -0600

    Added scene for Win Wheels

---
+++ DrawPokerIsh/Assets/Scripts/Controller/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene( "Main Menu" );
+    }
+
+
+    public void GotoPickX()
+    {
+        SceneManager.LoadScene( "PickOneHand" );
+    }
+
+    public void GotoMatchX()
+    {
+        SceneManager.LoadScene( "MatchX" );
+    }
+
+    public void GotoWinWheels()
+    {
+        SceneManager.LoadScene( "Win Wheels" );
+    }
+}
